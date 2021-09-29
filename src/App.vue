7e95a58cbd3182c0ea6dcaa9521085de589e8ce7
Paul <paul@meetric.app>
2021-09-29 22:32:22 +1000

    remove todos and store todos in localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <CreateTask @add-task="addTask" /><br /><br />
+  <div v-bind:key="task" v-for="(task, index) in tasks">
+    <input type="checkbox" />
+    {{ task }}
+    <button v-on:click="removeTask(index)">x</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CreateTask from "./components/CreateTask.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CreateTask,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("tasks")) {
+      try {
+        this.tasks = JSON.parse(localStorage.getItem("tasks"));
+      } catch (e) {
+        localStorage.removeItem("tasks");
+      }
+    }
+  //   this._keyListener = function (e) {
+  //     if (e.altKey && e.keyCode === 65) {
+  //       this.addTask("hello");
+  //     }
+  //   };
+  //   document.addEventListener("keydown", this._keyListener.bind(this));
+  // },
+  // beforeDestroy() {
+  //   document.removeEventListener("keydown", this._keyListener);
+  },
+  methods: {
+    addTask(e) {
+      this.tasks.push(e);
+      this.saveTasks();
+    },
+    removeTask(e) {
+      this.tasks.splice(e, 1);
+      this.saveTasks();
+    },
+    saveTasks() {
+      const parsed = JSON.stringify(this.tasks);
+      localStorage.setItem("tasks", parsed);
+    },
+  },
+};
 </script>
---
+++ src/components/CreateTask.vue
@@ -0,0 +1,31 @@
+<template>
+    <input
+      type="text"
+      id="taskinput"
+      v-model="task"
+      v-on:keyup.enter="createTask"
+      v-on:keyup.esc="removeFocus"
+    >
+    <button @click="createTask">Create</button>
+</template>
+
+<script>
+export default {
+  name: 'CreateTask',
+  emits: ["add-task"],
+  data() {
+    return {
+      task: "",
+    }
+  },
+  methods: {
+    createTask() {
+      this.$emit("add-task", this.task);
+      this.task = "";
+    },
+    removeFocus() {
+      taskinput.blur();
+    }
+  }
+}
+</script>
