91e4bf9495df222f4851bd7c5203ddac52adf81c
Paul <paul@meetric.app>
2021-11-05 16:20:51 +0100

    pre-loaded Giphy before completion

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <input
       type="checkbox" 
       v-model="task.status" 
-      v-on:change="completeTask(task.status)" 
+      v-on:change="changeTaskStatus(task.status)" 
     />
     <input 
       type="text" 
@@ -17,7 +17,8 @@
     />
     <button v-on:click="removeTask(index)">x</button>
   </div>
-  <img v-bind:src="giphyImgUrl" />
+  <img v-show="giphyToggle" v-bind:src="giphyImgSrcA" />
+  <img v-show="!giphyToggle" v-bind:src="giphyImgSrcB" />
 </template>
 
 <script>
@@ -36,13 +37,20 @@ export default {
       tasks: [],
       taskId: 0,
       giphyKey: "ifCV6DKdidYXRwUv5d2OEyVIl7Y2Fg8u",
+      giphyKeyword: "puppy",
       giphyEndpoint: "http://api.giphy.com/v1/gifs/search?",
       giphyPosition: 0,
-      giphyImgUrl: null
+      giphyImgSrc: null,
+      giphyImgSrcA: null,
+      giphyImgSrcB: null,
+      giphyToggle: false,
     };  
   },
 
   mounted() {
+    
+    this.loadGiphy();
+
     if (localStorage.taskId) {
       try {
         this.tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -51,20 +59,24 @@ export default {
         localStorage.removeItem("tasks");
       }
     }
-    this._keyListener = function (e) {
-      if (e.keyCode == 32) {
-        let nextUncheck = this.tasks.findIndex((task) => task.status == false);
-        if (nextUncheck > -1) {
-          this.tasks[nextUncheck].status = true;
-        }
-      }
-    };
-    document.addEventListener("keydown", this._keyListener.bind(this));
+    // Listen to SPACE key to complete an to-do
+    // Problem: this listener is also on while creating a new to-do
+    // this._keyListener = function (e) {
+    //   if (e.keyCode == 32) {
+    //     let nextUncheck = this.tasks.findIndex((task) => task.status == false);
+    //     if (nextUncheck > -1) {
+    //       this.tasks[nextUncheck].status = !this.tasks[nextUncheck].status;
+    //       this.changeTaskStatus(this.tasks[nextUncheck].status);
+    //     }
+    //   }
+    // };
+    // document.addEventListener("keydown", this._keyListener.bind(this));
   },
 
-  beforeDestroy() {
-    document.removeEventListener("keydown", this._keyListener);
-  },
+  // Destroy the key event listener
+  // beforeDestroy() {
+  //   document.removeEventListener("keydown", this._keyListener);
+  // },
 
   methods: {
 
@@ -72,29 +84,43 @@ export default {
       let taskObject = { id: this.taskId++, name: e, status: false };
       this.tasks.push(taskObject);
       this.saveTasks();
+      console.log("addTask");
     },
 
     removeTask(e) {
       this.tasks.splice(e, 1);
       this.saveTasks();
+      console.log("remoteTask");
     },
 
-    completeTask(e) {
-      this.saveTasks();
-      if (e) {
-        this.giphyPosition = Math.floor(Math.random() * 100);
-        axios
-          .get(this.giphyEndpoint+'q=puppy&limit=1&offset='+this.giphyPosition+'&api_key='+this.giphyKey)
-          .then(response => (this.giphyImgUrl = response.data.data[0].images.original.url));
-      }
-    },
-    
     saveTasks() {
       const parsed = JSON.stringify(this.tasks);
       localStorage.setItem("tasks", parsed);
       localStorage.taskId = this.taskId;
-      console.log("saved");
+      console.log("saveTasks");
     },
+
+    changeTaskStatus(e) {
+      this.saveTasks();
+      if (e) { 
+        this.loadGiphy(); 
+      }
+      console.log("changeTaskStatus");
+    },
+
+    async loadGiphy() {
+      this.giphyToggle = !this.giphyToggle;
+      this.giphyPosition = Math.floor(Math.random() * 100);
+      await axios
+        .get(this.giphyEndpoint+'q='+this.giphyKeyword+'&limit=1&offset='+this.giphyPosition+'&api_key='+this.giphyKey)
+        .then(response => (this.giphyImgSrc = response.data.data[0].images.fixed_height.webp));
+      if (this.giphyToggle == false) {
+        this.giphyImgSrcA = this.giphyImgSrc;
+      } else {
+        this.giphyImgSrcB = this.giphyImgSrc;
+      }
+      console.log("loadGiphy");
+    }
   },
 };
 </script>
